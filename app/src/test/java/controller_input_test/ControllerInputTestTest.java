8374b4d0a116ce220494a46babd8785850a7e9e4
Taekbeom Yoo <ytbeom@gmail.com>
2021-08-01 18:44:35 +0900

    Refactor #2: remove JRE

--- app/src/test/java/controller_input_test/ControllerInputTestTest.java
+++ app/src/test/java/controller_input_test/ControllerInputTestTest.java
@@ -7,7 +7,5 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class ControllerInputTestTest {
-    @Test void skippableTest() {
-        Assertions.assertTrue(true);
-    }
+
 }
